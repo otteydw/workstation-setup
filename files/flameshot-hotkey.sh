b4dcf55b770a8eddb580fa5479fb945a6e15a3fa
Dan Ottey <dottey@boomi.com>
2021-11-18 13:42:44 -0500

    Support for Ubuntu 20 in the Flameshot skeybinding script

--- files/flameshot-hotkey.sh
+++ files/flameshot-hotkey.sh
@@ -5,6 +5,7 @@
 # Release the PrtScr binding by this command:
 grep -q "^Ubuntu 18" /etc/issue && gsettings set org.gnome.settings-daemon.plugins.media-keys screenshot ''
 grep -q "^Ubuntu 19" /etc/issue && gsettings set org.gnome.settings-daemon.plugins.media-keys screenshot '[]'
+grep -q "^Ubuntu 20" /etc/issue && gsettings set org.gnome.settings-daemon.plugins.media-keys screenshot '[]'
 
 # Set new custom binding:
 
